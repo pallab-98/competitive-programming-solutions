7f2095de2846c0b87939f71681af9466ca354a5a
Pallab <mazharulislampallab98@gmail.com>
2026-06-19 20:48:23 +0600

    update

---
+++ A_Grade_Allocation.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int caseTest;
+    cin >> caseTest;
+
+    while (caseTest--)
+    {
+        int n, m;
+        cin >> n >> m;
+
+        long long sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+            sum += x;
+        }
+
+        cout << min(sum, (long long)m) << endl;
+    }
+
+    return 0;
+}
