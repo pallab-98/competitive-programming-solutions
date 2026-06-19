8daefcb3e115b7fc0977a9af2cac5bd047ebee75
Pallab <mazharulislampallab98@gmail.com>
2026-06-19 12:58:49 +0600

    update passing_marks

--- Passing_Marks.cpp
+++ Passing_Marks.cpp
@@ -14,11 +14,9 @@ int main()
         int n, x;
         cin >> n >> x;
         vector<int> a(n);
-        for (int i = 0; i < n; i++) 
+        for (int i = 0; i < n; i++)
             cin >> a[i];
-        
-        
-        
+
         sort(a.begin(), a.end());
 
         cout << a[n - x] - 1 << endl;
