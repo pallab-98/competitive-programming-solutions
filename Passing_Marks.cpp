2af386248c9749c61077923bcc09a8eca03fe548
Pallab <mazharulislampallab98@gmail.com>
2026-06-19 12:35:20 +0600

    codechef update

--- Passing_Marks.cpp
+++ Passing_Marks.cpp
@@ -1,25 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    cout.tie(nullptr);
 
     int caseTest;
     cin >> caseTest;
-    while (caseTest--) {
+    while (caseTest--)
+    {
         int n, x;
         cin >> n >> x;
         vector<int> a(n);
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             cin >> a[i];
         }
-        
-        
+
         sort(a.begin(), a.end());
-        
+
         cout << a[n - x] - 1 << endl;
-        
     }
     return 0;
 }
