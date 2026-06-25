fb25ecb14c0ed07c331f5f55cf594072a9c262fa
Pallab <mazharulislampallab98@gmail.com>
2026-06-25 23:41:45 +0600

    update

--- A_Reorder.cpp
+++ A_Reorder.cpp
@@ -1,31 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    
+
     int caseTest;
     cin >> caseTest;
 
-    while (caseTest--) {
+    while (caseTest--)
+    {
         int n;
         long long m;
         cin >> n >> m;
 
         long long sum = 0;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             long long x;
             cin >> x;
             sum += x;
         }
 
         if (sum == m)
-            cout << "YES"<<endl;
+            cout << "YES" << endl;
         else
-            cout << "NO"<<endl;
+            cout << "NO" << endl;
     }
 
     return 0;
