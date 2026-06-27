1bfdff8dd7e9d17d611f9006045a830f27fbf5db
Pallab <mazharulislampallab98@gmail.com>
2026-06-27 22:20:44 +0600

    update

---
+++ A_Red_and_Blue_Beans.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        ll r, b, d;
+        cin >> r >> b >> d;
+
+        ll mn = min(r, b);
+        ll mx = max(r, b);
+
+        if (mx <= mn * (d + 1))
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
