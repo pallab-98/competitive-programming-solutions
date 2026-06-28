5da32678b2c3b4d7fa437cc05949ba947881024a
Pallab <mazharulislampallab98@gmail.com>
2026-06-28 16:32:06 +0600

    update

---
+++ A_Odd_Set.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+   
+
+    int caseTest;
+    cin >> caseTest;
+    while (caseTest--)
+    {
+        int loop;
+        cin >> loop;
+        int even, odd, i;
+        even = odd = 0;
+        i = 1;
+        while (i <= loop * 2)
+        {
+            int value;
+            cin >> value;
+            if (value & 1)
+                odd++;
+            else
+                even++;
+            i++;
+        }
+        cout << ((odd == even) ? "Yes" : "No") << endl;
+    }
+    return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -2,7 +2,22 @@
 using namespace std;
 int main()
 {
-    cout << "mazharul islam pallab" << endl;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
+    int caseTest, key = 0;
+    cin >> caseTest;
+    while (caseTest--)
+    {
+        int key;
+        cin >> key;
+        key = key;
+        int value, count = 0;
+        cin >> value;
+        if (value % 2 == 0)
+            count++;
+    }
+    cout << (key == count ? "Yes" : "No") << endl;
     return 0;
 }
