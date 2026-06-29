f90ddbb4c2658ddcfe3f9723b330546fdd96ae52
Pallab <mazharulislampallab98@gmail.com>
2026-06-29 22:24:51 +0600

    Upate

---
+++ A_Casimir_s_String_Solitaire.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        string s;
+        cin >> s;
+
+        int a = 0, b = 0, c = 0;
+
+        for (char ch : s) {
+            if (ch == 'A') a++;
+            else if (ch == 'B') b++;
+            else c++;
+        }
+
+        if (b == a + c)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
