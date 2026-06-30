3d350ae2de61ae6e2ebc2766291db34e8d4a5bbf
Pallab <mazharulislampallab98@gmail.com>
2026-06-30 21:47:20 +0600

    update

--- B_Good_times_Good_times.cpp
+++ B_Good_times_Good_times.cpp
@@ -1,16 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool good(long long n) {
-    bool vis[10] = {0};
+bool isGood(long long n) {
+    bool seen[10] = {false};
     int cnt = 0;
-
-    if (n == 0) return true;
 
     while (n > 0) {
         int d = n % 10;
-        if (!vis[d]) {
-            vis[d] = true;
+        if (!seen[d]) {
+            seen[d] = true;
             cnt++;
         }
         n /= 10;
@@ -22,6 +20,7 @@ bool good(long long n) {
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    cout.tie(nullptr);
 
     int t;
     cin >> t;
@@ -32,14 +31,17 @@ int main() {
 
         bool found = false;
 
-        for (int d = 1; d <= 9 && !found; d++) {
+        for (int digit = 1; digit <= 9 && !found; digit++) {
             long long y = 0;
-            for (int len = 1; len <= 10; len++) {
-                y = y * 10 + d;
-                if (y > 1000000000LL) break;
 
-                if (good(x * y)) {
-                    cout << y << "\n";
+            for (int len = 1; len <= 10; len++) {
+                y = y * 10 + digit;
+
+                if (y > 1000000000LL)
+                    break;
+
+                if (isGood(x * y)) {
+                    cout << y << '\n';
                     found = true;
                     break;
                 }
