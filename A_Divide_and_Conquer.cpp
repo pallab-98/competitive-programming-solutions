f9a8b092739bc1fc2a2907bcb2e0ec74ec9b6d3a
Pallab <mazharulislampallab98@gmail.com>
2026-06-30 21:27:44 +0600

    Update

--- A_Divide_and_Conquer.cpp
+++ A_Divide_and_Conquer.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    cou.tie(nullptr);
+    cout.tie(nullptr);
 
     int caseTest;
     cin >> caseTest;
 
-    while (caseTest--) {
+    while (caseTest--)
+    {
         int x, y;
         cin >> x >> y;
 
         if (x == y)
-            cout << "YES"<<endl;
+            cout << "YES" << endl;
         else if (y > 1 && x % y == 0)
-            cout << "YES"<<endl;
+            cout << "YES" << endl;
         else
-            cout << "NO"<<endl;
+            cout << "NO" << endl;
     }
 
     return 0;
-} 
+}
