79d0d1f41699f407ad7d8389d8cd4f3f4f2c0865
Pallab <mazharulislampallab98@gmail.com>
2026-06-30 23:51:28 +0600

    update

--- B_Equal_Candies.cpp
+++ B_Equal_Candies.cpp
@@ -1,30 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int caseTest;
     cin >> caseTest;
 
-    while (caseTest--) {
+    while (caseTest--)
+    {
         int n;
         cin >> n;
 
         vector<int> a(n);
         int mn = INT_MAX;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             cin >> a[i];
             mn = min(mn, a[i]);
         }
 
         long long ans = 0;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
             ans += (a[i] - mn);
-        }
 
         cout << ans << '\n';
     }
