e6957f9c40a278ac6b1546a7a2f95c501dd07acc
Pallab <mazharulislampallab98@gmail.com>
2026-07-01 20:57:10 +0600

    Update

--- Equivalent_Exchange.cpp
+++ Equivalent_Exchange.cpp
@@ -1,18 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     int caseTest;
     cin >> caseTest;
 
-    while (caseTest--) {
+    while (caseTest--)
+    {
         int N, K;
         cin >> N >> K;
 
         int prefix = 0;
         int mn = 0, mx = 0;
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++)
+        {
             int x;
             cin >> x;
             prefix += x;
@@ -21,9 +24,9 @@ int main() {
         }
 
         if (mx - mn <= K)
-            cout << "Yes\n";
+            cout << "Yes" << endl;
         else
-            cout << "No\n";
+            cout << "No" << endl;
     }
 
     return 0;
