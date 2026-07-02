a21677502597221cb36ec82e08ce39465035fef8
Pallab <mazharulislampallab98@gmail.com>
2026-07-02 20:40:33 +0600

    Codeforces Prbolem Solve 02 , 02/07/26

--- A_Greg_s_Workout.cpp
+++ A_Greg_s_Workout.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 
 int main()
-    {ios::sync_with_stdio(false);
-        cin.tie(nullptr);
-        cout.tie(nullptr);
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
     int n;
     cin >> n;
 
