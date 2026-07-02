e2f5935723c488515292c24bd72c3bf5d4e8f0c1
Pallab <mazharulislampallab98@gmail.com>
2026-07-02 20:39:08 +0600

    Codeforces Prbolem Solve 03 , 02/07/26

---
+++ A_Greg_s_Workout.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int chest = 0, biceps = 0, back = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+
+        if (i % 3 == 0)
+            chest += x;
+        else if (i % 3 == 1)
+            biceps += x;
+        else
+            back += x;
+    }
+
+    if (chest > biceps && chest > back)
+        cout << "chest";
+    else if (biceps > chest && biceps > back)
+        cout << "biceps";
+    else
+        cout << "back";
+
+    return 0;
+}
