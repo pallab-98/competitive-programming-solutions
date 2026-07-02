576e69350110c46c1e67cabc5719db8267faa56f
Pallab <mazharulislampallab98@gmail.com>
2026-07-02 20:31:49 +0600

    Codeforces Problem solve:  02  02/07/26

---
+++ A_Cupboards.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int left0 = 0, left1 = 0;
+    int right0 = 0, right1 = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int l, r;
+        cin >> l >> r;
+
+        if (l == 0)
+            left0++;
+        else
+            left1++;
+
+        if (r == 0)
+            right0++;
+        else
+            right1++;
+    }
+
+    cout << min(left0, left1) + min(right0, right1) << endl;
+
+    return 0;
+}
