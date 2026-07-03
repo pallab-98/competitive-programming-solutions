d0cf8064e02f3f54d65bd8e4725ad317aad84628
Pallab <mazharulislampallab98@gmail.com>
2026-07-03 20:01:56 +0600

    CodeForces Problme Solution 02 ,  03/07/26

---
+++ A_Colorful_Stones_Simplified_Edition.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    string s, t;
+    cin >> s >> t;
+
+    int pos = 0;
+
+    for (char c : t) {
+        if (c == s[pos])
+            pos++;
+    }
+
+    cout << pos + 1 << '\n';
+
+    return 0;
+}
