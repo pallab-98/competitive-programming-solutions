d9fd7b47ba1b8b0772906b1298550ac1ea606c36
Pallab <mazharulislampallab98@gmail.com>
2026-07-03 20:09:43 +0600

    CodeForces Problem Solve 03, 03/07/26

---
+++ A_Shaass_and_Oskols.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<int> a(n + 1);
+
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+
+    int m;
+    cin >> m;
+
+    while (m--) {
+        int x, y;
+        cin >> x >> y;
+
+        if (x > 1)
+            a[x - 1] += y - 1;
+
+        if (x < n)
+            a[x + 1] += a[x] - y;
+
+        a[x] = 0;
+    }
+
+    for (int i = 1; i <= n; i++)
+        cout << a[i] << '\n';
+
+    return 0;
+}
