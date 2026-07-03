36a52aff75b628552513984611680ac66a00f451
Pallab <mazharulislampallab98@gmail.com>
2026-07-03 20:15:35 +0600

    CodeChef Problem Solve 01, 03/07/26

---
+++ Addition_and_multiplication.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int A, B, S, P;
+        cin >> A >> B;
+        
+        S = A + B;
+        
+        P = A * B;
+        
+        cout<< S << " " << P << endl;
+    }
+    return 0;
+}
