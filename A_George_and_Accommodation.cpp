d0dfab663cc21b203ab8e41b14272907c96641f1
Pallab <mazharulislampallab98@gmail.com>
2026-07-04 20:37:07 +0600

    CodeForces Problem solve 03, 04/07/26

---
+++ A_George_and_Accommodation.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    int ans = 0;
+
+    while (n--)
+    {
+        int p, q;
+        cin >> p >> q;
+
+        if (q - p >= 2)
+            ans++;
+    }
+
+    cout << ans << '\n';
+
+    return 0;
+}
