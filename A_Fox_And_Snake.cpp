aeb488fca37e7539a687b2b259d44844fb13d3d6
Pallab <mazharulislampallab98@gmail.com>
2026-07-04 20:32:46 +0600

    CodeForces Problem Sovlve 02, 04/07/26

---
+++ A_Fox_And_Snake.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    bool right = true;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            for (int j = 0; j < m; j++)
+                cout << '#';
+        }
+        else
+        {
+            if (right)
+            {
+                for (int j = 0; j < m - 1; j++)
+                    cout << '.';
+                cout << '#';
+            }
+            else
+            {
+                cout << '#';
+                for (int j = 0; j < m - 1; j++)
+                    cout << '.';
+            }
+
+            right = !right;
+        }
+
+        cout << '\n';
+    }
+
+    return 0;
+}
