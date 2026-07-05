f7ed2355bdf9ed740cf2f6bc9fa9cfa1fb92494b
Pallab <mazharulislampallab98@gmail.com>
2026-07-05 23:21:43 +0600

    CodeForces Problem Solve 02, 06/07/26

---
+++ A_Bulbs.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    bool bulb[105] = {false};
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+
+        while (x--)
+        {
+            int y;
+            cin >> y;
+            bulb[y] = true;
+        }
+    }
+
+    for (int i = 1; i <= m; i++)
+    {
+        if (!bulb[i])
+        {
+            cout << "NO\n";
+            return 0;
+        }
+    }
+
+    cout << "YES\n";
+}
