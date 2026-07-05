da47a9bf3675a8c086fce0ea0782b2638fc3a5cd
Pallab <mazharulislampallab98@gmail.com>
2026-07-05 23:19:42 +0600

    CodeForces Problem Sovle 01, 06/07/26

--- A_Johny_Likes_Numbers.cpp
+++ A_Johny_Likes_Numbers.cpp
@@ -6,11 +6,11 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    
+
     long long n, k;
     cin >> n >> k;
 
-    cout << (n / k + 1) * k << '\n';
+    cout << (n / k + 1) * k << endl;
 
     return 0;
 }
