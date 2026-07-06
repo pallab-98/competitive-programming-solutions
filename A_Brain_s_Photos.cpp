321c6e8ac9328d7735925a45cbd98cb8e54bf7db
Pallab <mazharulislampallab98@gmail.com>
2026-07-06 22:59:19 +0600

    CodeForces Problem Solve 02, 06/07/26

---
+++ A_Brain_s_Photos.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    bool color = false;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            char ch;
+            cin >> ch;
+
+            if (ch == 'C' || ch == 'M' || ch == 'Y')
+                color = true;
+        }
+    }
+
+    if (color)
+        cout << "#Color";
+    else
+        cout << "#Black&White";
+
+    return 0;
+}
