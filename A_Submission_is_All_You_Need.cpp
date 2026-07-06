6ac7d2037cc2458bd27914f4c0d60081088718b7
Pallab <mazharulislampallab98@gmail.com>
2026-07-06 23:28:37 +0600

    CodeForces Problem Solve 03, 06/07/26

--- A_Submission_is_All_You_Need.cpp
+++ A_Submission_is_All_You_Need.cpp
@@ -17,7 +17,7 @@ int main()
         int sum = 0;
         bool hasZero = false;
 
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < len; i++)
         {
             int x;
             cin >> x;
