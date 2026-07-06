056c986cd958a2835b42a6a488441adfbc32f4a6
Pallab <mazharulislampallab98@gmail.com>
2026-07-06 23:02:48 +0600

    CodeForces Problem Solve 03, 06/07/26

---
+++ A_Submission_is_All_You_Need.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int caseTest;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int sum = 0;
+        bool hasZero = false;
+
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+            sum += x;
+
+            if (x == 0)
+                hasZero = true;
+        }
+
+        cout << sum + (hasZero ? 1 : 0) << '\n';
+    }
+
+    return 0;
+}
