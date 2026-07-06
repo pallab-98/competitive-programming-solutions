50e28c4a6f3b4103141d0aedb994d712919ac570
Pallab <mazharulislampallab98@gmail.com>
2026-07-06 22:54:00 +0600

    CodeForces Problem Solve 01, 06/07/26

---
+++ A_A_Good_Contest.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    bool ok = false;
+
+    while (n--) {
+        string name;
+        int before, after;
+
+        cin >> name >> before >> after;
+
+        if (before >= 2400 && after > before)
+            ok = true;
+    }
+
+    cout << (ok ? "YES" : "NO");
+
+    return 0;
+}
