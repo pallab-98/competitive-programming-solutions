e5e401aab044cd7d9b470a86c4f4fad88dcccac8
Pallab <mazharulislampallab98@gmail.com>
2026-07-08 23:03:53 +0600

    CodeForces Problem solving 01

--- C_Bun_Lover.cpp
+++ C_Bun_Lover.cpp
@@ -1,17 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
     int caseTest;
-    cin >> caseTest;
+    cin >> T;
 
-    while (caseTest--)
-    {
+    while (T--) {
         long long n;
         cin >> n;
 
-        cout << n * n + 3 << endl;
+        cout << n * n + 2 * n + 2 << endl;
     }
 
     return 0;
