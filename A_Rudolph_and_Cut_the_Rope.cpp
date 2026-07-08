4e15ab9cea103abfb4ec3a17e6598c518183c25c
Pallab <mazharulislampallab98@gmail.com>
2026-07-08 23:05:59 +0600

    CodeForces Problem solve 03, 08/07/26

---
+++ A_Rudolph_and_Cut_the_Rope.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int caseTest;
+    cin >> T;
+
+    while (T--) {
+        int n;
+        cin >> n;
+
+        int ans = 0;
+
+        while (n--) {
+            int a, b;
+            cin >> a >> b;
+
+            if (a > b)
+                ans++;
+        }
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
