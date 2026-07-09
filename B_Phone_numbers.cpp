f54f5290cb577960b3678c93775c2687996f8a86
Pallab <mazharulislampallab98@gmail.com>
2026-07-09 22:51:17 +0600

    CodeForces Problem Solve 02, 08/07/26

--- B_Phone_numbers.cpp
+++ B_Phone_numbers.cpp
@@ -1,32 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    
+
     int len;
     cin >> len;
 
     string str;
     cin >> str;
 
-    if (n % 2 == 0) {
-        for (int i = 0; i < len; i += 2) {
+    if (len % 2 == 0)
+    {
+        for (int i = 0; i < len; i += 2)
+        {
             cout << str[i] << str[i + 1];
             if (i + 2 < len)
                 cout << "-";
         }
-    } else {
+    }
+    else
+    {
         int i = 0;
 
-        while (i < len - 3) {
+        while (i < len - 3)
+        {
             cout << str[i] << str[i + 1] << "-";
             i += 2;
         }
 
-        cout << s.substr(len - 3);
+        cout << str.substr(len - 3);
     }
 
     return 0;
