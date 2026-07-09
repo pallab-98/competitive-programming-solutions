ebc8db94716195f79d929cc84c7d6be0df3db744
Pallab <mazharulislampallab98@gmail.com>
2026-07-09 22:36:17 +0600

    Codeforces Problem Sove 01, 08/07/26

---
+++ A_Jagged_Swaps.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+
+        if (a[0] == 1)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
