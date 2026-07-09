5660bae0741dc931b71d194cec2b3155459c6fd7
Pallab <mazharulislampallab98@gmail.com>
2026-07-09 22:36:05 +0600

    Codeforces Problem Sove 01, 08/07/26

--- A_Rudolph_and_Cut_the_Rope.cpp
+++ A_Rudolph_and_Cut_the_Rope.cpp
@@ -1,21 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
+
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    
+
     int caseTest;
     cin >> caseTest;
 
-    while (caseTest--) {
+    while (caseTest--)
+    {
         int n;
         cin >> n;
 
         int ans = 0;
 
-        while (n--) {
+        while (n--)
+        {
             int a, b;
             cin >> a >> b;
 
