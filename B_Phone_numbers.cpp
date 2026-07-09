f3b6bce0500aecb9d2481c25d1d6cf7b5b7adbf4
Pallab <mazharulislampallab98@gmail.com>
2026-07-09 22:47:20 +0600

    CodeForces Problem Solve 02, 08/07/26

---
+++ B_Phone_numbers.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    if (n % 2 == 0) {
+        for (int i = 0; i < n; i += 2) {
+            cout << s[i] << s[i + 1];
+            if (i + 2 < n)
+                cout << "-";
+        }
+    } else {
+        int i = 0;
+
+        while (i < n - 3) {
+            cout << s[i] << s[i + 1] << "-";
+            i += 2;
+        }
+
+        cout << s.substr(n - 3);
+    }
+
+    return 0;
+}
