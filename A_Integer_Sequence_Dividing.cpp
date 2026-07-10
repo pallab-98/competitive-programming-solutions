a8898ae8170ed4b915db00102680aeec2fde3a09
Pallab <mazharulislampallab98@gmail.com>
2026-07-10 23:14:47 +0600

    CodeForces Problem Solve 03, 10/07/26

--- A_Integer_Sequence_Dividing.cpp
+++ A_Integer_Sequence_Dividing.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
@@ -18,4 +19,3 @@ int main() {
 
     return 0;
 }
-
