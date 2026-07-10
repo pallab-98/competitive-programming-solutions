6e033bcd6456ffb50191615bdb0e4cc86395409f
Pallab <mazharulislampallab98@gmail.com>
2026-07-10 23:13:28 +0600

    CodeForces Problem Solve 03, 10/07/26

--- A_Integer_Sequence_Dividing.cpp
+++ A_Integer_Sequence_Dividing.cpp
@@ -1,2 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    long long n;
+    cin >> n;
+
+    long long sum = n * (n + 1) / 2;
+
+    if (sum % 2 == 0)
+        cout << 0 << '\n';
+    else
+        cout << 1 << '\n';
+
+    return 0;
+}
 
