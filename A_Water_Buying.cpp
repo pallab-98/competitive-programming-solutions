bf9834de49a82958c3983e931a8d70ab611fb397
Pallab <mazharulislampallab98@gmail.com>
2026-07-11 23:20:20 +0600

    CodeForces Problem Solve 03, 10/07/26

---
+++ A_Water_Buying.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int q;
+    cin >> q;
+
+    while (q--)
+    {
+        long long n, a, b;
+        cin >> n >> a >> b;
+
+        if (2 * a <= b)
+        {
+            cout << n * a << "\n";
+        }
+        else
+        {
+            cout << (n / 2) * b + (n % 2) * a << "\n";
+        }
+    }
+
+    return 0;
+}
