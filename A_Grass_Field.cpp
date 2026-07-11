27495b12d1a65fcf808b8b8e662b14c3f4a67095
Pallab <mazharulislampallab98@gmail.com>
2026-07-11 22:26:02 +0600

    CodeForces Problem Solve 02, 11/07/26

---
+++ A_Grass_Field.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int caset;
+    cin >> t;
+
+    while (t--)
+    {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+
+        int sum = a + b + c + d;
+
+        if (sum == 0)
+            cout << 0 << "\n";
+        else if (sum == 4)
+            cout << 2 << "\n";
+        else
+            cout << 1 << "\n";
+    }
+
+    return 0;
+}
