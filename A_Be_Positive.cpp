61ae6a3227d3b5790c37639eee8f6fd20f4074e3
Pallab <mazharulislampallab98@gmail.com>
2026-07-12 19:47:37 +0600

    CodeForces Problem solve 01, 12/07/26

---
+++ A_Be_Positive.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+    int n;
+    cin >> n;
+
+    int pos = 0, neg = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+
+        if (x > 0)
+            pos++;
+        else if (x < 0)
+            neg++;
+    }
+
+    int need = (n + 1) / 2;
+
+    if (pos >= need)
+        cout << 1 << endl;
+    else if (neg >= need)
+        cout << -1 << endl;
+    else
+        cout << 0 << endl;
+
+    return 0;
+}
