d0334244adc309b6df36e6b0d86b380935ddff60
Pallab <mazharulislampallab98@gmail.com>
2026-07-12 19:55:56 +0600

    CodeForces Problem Solve 02, 12/07/26

--- A_Sea_Battle.cpp
+++ A_Sea_Battle.cpp
@@ -1,10 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
+
     int w1, h1, w2, h2;
     cin >> w1 >> h1 >> w2 >> h2;
 
