d52547a8f4770f7e5e6fee8a3e7fa498d3b5eb4f
Pallab <mazharulislampallab98@gmail.com>
2026-07-12 19:54:56 +0600

    CodeForces Problem Solve 02, 12/07/26

---
+++ A_Sea_Battle.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int w1, h1, w2, h2;
+    cin >> w1 >> h1 >> w2 >> h2;
+
+    cout << 2 * h1 + 2 * h2 + 2 * w1 + 4;
+
+    return 0;
+}
