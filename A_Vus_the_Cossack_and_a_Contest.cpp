0438be449755b0b7c94b43badf288ac930cfad3e
Pallab <mazharulislampallab98@gmail.com>
2026-07-13 21:51:38 +0600

    CodeForces Problem Solve 03, 13/07/26

---
+++ A_Vus_the_Cossack_and_a_Contest.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m, k;
+    cin >> n >> m >> k;
+
+    if (m >= n && k >= n)
+        cout << "Yes\n";
+    else
+        cout << "No\n";
+
+    return 0;
+}
