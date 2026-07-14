dc0c879bd645186590682ceaa66295762b23f3ea
Pallab <mazharulislampallab98@gmail.com>
2026-07-14 23:18:35 +0600

    Codeforces Problem solve 01, 13/07/27

--- A_CME.cpp
+++ A_CME.cpp
@@ -14,7 +14,7 @@ int main() {
         cin >> n;
 
         if (n == 2)
-            cout << 2 << '\n';
+            cout << 2 << endl;
         else if (n % 2 == 0)
             cout << 0 << '\n';
         else
