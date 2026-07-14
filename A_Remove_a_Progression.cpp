faa4a2f1fd0ff5f4d10a17daa2a48dcf33219f61
Pallab <mazharulislampallab98@gmail.com>
2026-07-14 23:36:28 +0600

    CodeForces Problem solve 02, 14/07/27

---
+++ A_Remove_a_Progression.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        long long n, x;
+        cin >> n >> x;
+
+        cout << 2 * x << '\n';
+    }
+
+    return 0;
+}
