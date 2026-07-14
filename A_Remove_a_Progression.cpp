772858e56477e091d0c14c11ec343acee33e85fc
Pallab <mazharulislampallab98@gmail.com>
2026-07-14 23:37:54 +0600

    CodeForces Problem solve 02, 14/07/27

--- A_Remove_a_Progression.cpp
+++ A_Remove_a_Progression.cpp
@@ -1,14 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
     int caseTest;
     cin >> caseTest;
 
-    while (caseTest--) {
+    while (caseTest--)
+    {
         long long n, x;
         cin >> n >> x;
 
