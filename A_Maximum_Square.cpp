0ed3989bd6a25c740d1641acd8ea2cba66c3f827
Pallab <mazharulislampallab98@gmail.com>
2026-07-15 21:59:05 +0600

    CodeForces Problem Solve 02, 14/07/27

--- A_Maximum_Square.cpp
+++ A_Maximum_Square.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
@@ -9,24 +10,25 @@ int main() {
     int caseTest;
     cin >> caseTest;
 
-    while (caseTest--) {
+    while (caseTest--)
+    {
         int len;
         cin >> len;
 
         vector<int> a(len);
 
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < len; i++)
             cin >> a[i];
 
         sort(a.begin(), a.end(), greater<int>());
 
         int ans = 0;
 
-        for (int i = 0; i < n; i++) {
-            ans = max(ans, min(a[i], i + 1));
-        }
+        for (int i = 0; i < len; i++)
 
-        cout << ans << '\n';
+            ans = max(ans, min(a[i], i + 1));
+
+        cout << ans << endl;
     }
 
     return 0;
