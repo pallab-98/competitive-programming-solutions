68c4cdc3618d172d8d57a9f8e41c1908a30491f3
Pallab <mazharulislampallab98@gmail.com>
2026-07-15 21:35:19 +0600

    CodeForces Problem Solve 01, 14/07/27

---
+++ A_Maximum_Square.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+
+        sort(a.begin(), a.end(), greater<int>());
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            ans = max(ans, min(a[i], i + 1));
+        }
+
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
