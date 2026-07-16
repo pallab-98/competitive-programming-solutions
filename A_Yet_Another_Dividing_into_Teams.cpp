d68d0446870c58629d059003fbb532c6200d6e6b
Pallab <mazharulislampallab98@gmail.com>
2026-07-16 11:07:45 +0600

    Codeforces Problem Solve 03, 14/07/26

---
+++ A_Yet_Another_Dividing_into_Teams.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+
+        sort(a.begin(), a.end());
+
+        bool needTwoTeams = false;
+
+        for (int i = 1; i < n; i++) {
+            if (a[i] - a[i - 1] == 1) {
+                needTwoTeams = true;
+                break;
+            }
+        }
+
+        cout << (needTwoTeams ? 2 : 1) << '\n';
+    }
+
+    return 0;
+}
