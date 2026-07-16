b06d5a7898a25c0ace53347d4704185be781efe9
Pallab <mazharulislampallab98@gmail.com>
2026-07-16 12:04:28 +0600

    CodeForces Problem Solve 01, 16/07/26

---
+++ A_Dead_Pixel.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int a, b, x, y;
+        cin >> a >> b >> x >> y;
+
+        int ans = max({
+            x * b,
+            (a - x - 1) * b,
+            a * y,
+            a * (b - y - 1)
+        });
+
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
