f36813986e3ec92e404e9fe183414798d9dc7b9b
Pallab <mazharulislampallab98@gmail.com>
2026-07-17 11:59:57 +0600

    CodeForces Problem Solve 01, 17/07/26

--- A_Dead_Pixel.cpp
+++ A_Dead_Pixel.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
@@ -9,16 +10,15 @@ int main() {
     int caseTest;
     cin >> caseTest;
 
-    while (caseTest--) {
+    while (caseTest--)
+    {
         int a, b, x, y;
         cin >> a >> b >> x >> y;
 
-        int ans = max({
-            x * b,
-            (a - x - 1) * b,
-            a * y,
-            a * (b - y - 1)
-        });
+        int ans = max({x * b,
+                       (a - x - 1) * b,
+                       a * y,
+                       a * (b - y - 1)});
 
         cout << ans << endl;
     }
