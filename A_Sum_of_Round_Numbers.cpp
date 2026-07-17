b8e38714804613c88499e1c6121980018a47495f
Pallab <mazharulislampallab98@gmail.com>
2026-07-17 14:20:33 +0600

    CodeForces Problem Solve 02, 17/07/26

--- A_Sum_of_Round_Numbers.cpp
+++ A_Sum_of_Round_Numbers.cpp
@@ -1,9 +1,39 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
+    int t;
+    cin >> t;
 
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> ans;
+        int place = 1;
+
+        while (n > 0)
+        {
+            int digit = n % 10;
+
+            if (digit != 0)
+                ans.push_back(digit * place);
+
+            place *= 10;
+            n /= 10;
+        }
+
+        cout << ans.size() << endl;
+        for (int x : ans)
+            cout << x << " ";
+
+        cout << "\n";
+    }
 
     return 0;
 }
