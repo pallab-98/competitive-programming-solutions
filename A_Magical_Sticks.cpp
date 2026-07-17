8bf370260eb23f2cbfb3e9dad6790926785a9eb1
Pallab <mazharulislampallab98@gmail.com>
2026-07-17 14:29:43 +0600

    Codeforces Problem Solve 02, 17/07/26

---
+++ A_Magical_Sticks.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int caseTest;
+    cin >>caseTest;
+
+    while (caseTest--)
+    {
+        long long n;
+        cin >> n;
+
+        cout << (n + 1) / 2 << endl;
+    }
+
+    return 0;
+}
