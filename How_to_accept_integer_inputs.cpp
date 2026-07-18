4d98b488342dc7d3dfff2d2ce4e2024631556403
Pallab <mazharulislampallab98@gmail.com>
2026-07-18 11:33:08 +0600

    CodeChef Problem Solve 02, 17/07/26

---
+++ How_to_accept_integer_inputs.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cint.tie(nullptr);
+    cout.tie(nullptr);
+
+    int number;
+    cin>>number;
+    cout<<number;
+
+
+
+
+    return 0;
+}
