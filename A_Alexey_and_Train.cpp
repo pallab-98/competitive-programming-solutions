9837e635c92e16b6d4e581f71b91254d042da88e
Pallab <mazharulislampallab98@gmail.com>
2026-07-18 11:46:49 +0600

    CodeForces Problem Solve 01, 17/07/26

--- A_Alexey_and_Train.cpp
+++ A_Alexey_and_Train.cpp
@@ -37,7 +37,7 @@ int main()
             
             if (i == n)
             {
-                cout << cur << '\n';
+                cout << cur << endl;
                 break;
             }
 
