9b16421ce30c6f9bdee241752351399419244693
Pallab <mazharulislampallab98@gmail.com>
2026-07-18 11:42:32 +0600

    CodeForces Problem Solve 01, 17/07/26

---
+++ A_Alexey_and_Train.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> a(n + 1), b(n + 1), tm(n + 1);
+
+        for (int i = 1; i <= n; i++)
+            cin >> a[i] >> b[i];
+
+        for (int i = 1; i <= n; i++)
+            cin >> tm[i];
+
+        int cur = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            
+            cur += tm[i];
+
+            
+            if (cur < a[i])
+                cur = a[i];
+
+            
+            if (i == n)
+            {
+                cout << cur << '\n';
+                break;
+            }
+
+            
+            int stay = (b[i] - a[i] + 1) / 2;
+            cur += stay;
+
+            cur = max(cur, b[i]);
+        }
+    }
+
+    return 0;
+}
