de4ade0f6669781819aa76864bd504a8bbcedabe
Pallab <mazharulislampallab98@gmail.com>
2026-07-18 11:34:32 +0600

    CodeChef Problem Solve 02, 17/07/26

--- How_to_accept_integer_inputs.cpp
+++ How_to_accept_integer_inputs.cpp
@@ -1,17 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     ios::sync_with_stdio(false);
-    cint.tie(nullptr);
+    cin.tie(nullptr);
     cout.tie(nullptr);
 
     int number;
-    cin>>number;
-    cout<<number;
-
-
-
+    cin >> number;
+    cout << number;
 
     return 0;
 }
