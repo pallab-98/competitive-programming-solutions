57503ee1d0214cd542d4053ecb3bca173de2b45e
Pallab <mazharulislampallab98@gmail.com>
2026-07-18 14:35:51 +0600

    CodeForces Problem Solve 03, 17/07/26

--- A_Direction_Change.cpp
+++ A_Direction_Change.cpp
@@ -5,11 +5,11 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    cout.tie(nullptr);
-    int caseTest;
-    cin >> caseTest;
 
-    while (caseTest--)
+    int t;
+    cin >> t;
+
+    while (t--)
     {
         long long n, m;
         cin >> n >> m;
@@ -17,23 +17,21 @@ int main()
         if (n == 1 && m == 1)
         {
             cout << 0 << '\n';
-            continue;
         }
-
-        if (min(n, m) == 1)
+        else if (min(n, m) == 1)
         {
             if (max(n, m) == 2)
                 cout << 1 << '\n';
             else
                 cout << -1 << '\n';
-
-            continue;
         }
+        else
+        {
+            long long mn = min(n, m);
+            long long mx = max(n, m);
 
-        long long x = min(n, m);
-        long long y = max(n, m);
-
-        cout << (2 * x - 2) + 2 * max(0LL, y - x - 1) << '\n';
+            cout << 2 * (mn - 1) + max(0LL, 2 * (mx - mn) - 1) << '\n';
+        }
     }
 
     return 0;
