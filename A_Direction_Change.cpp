91320a0b945c5e2dae72e9501823e3d6043f16a3
Pallab <mazharulislampallab98@gmail.com>
2026-07-18 14:32:23 +0600

    CodeForces Problem Solve 03, 17/07/26

---
+++ A_Direction_Change.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        long long n, m;
+        cin >> n >> m;
+
+        if (n == 1 && m == 1)
+        {
+            cout << 0 << '\n';
+            continue;
+        }
+
+        if (min(n, m) == 1)
+        {
+            if (max(n, m) == 2)
+                cout << 1 << '\n';
+            else
+                cout << -1 << '\n';
+
+            continue;
+        }
+
+        long long x = min(n, m);
+        long long y = max(n, m);
+
+        cout << (2 * x - 2) + 2 * max(0LL, y - x - 1) << '\n';
+    }
+
+    return 0;
+}
