b3ce69f9e0d1084db538bfc6ae6f262b5c5fdf3c
Pallab <mazharulislampallab98@gmail.com>
2026-07-18 11:23:04 +0600

    CodeChef Problem Solve 01, 17/07/26

---
+++ Data_Types.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+  int a = -5;
+  cout << a << endl;
+
+  float b = 4.0; 
+  cout << b << endl;
+
+  string s = "abcd";
+  cout << s << endl;
+
+  int arr[4] = { 7, 8, 9, 10 };
+  cout << arr[2];
+}
