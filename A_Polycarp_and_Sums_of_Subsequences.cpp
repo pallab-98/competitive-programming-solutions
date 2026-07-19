8a07b2d6a6941aa08298cf0777b70133c60bef67
Pallab <mazharulislampallab98@gmail.com>
2026-07-19 12:46:50 +0600

    CodeForces Problem Solve 01, 19/07/26

---
+++ A_Polycarp_and_Sums_of_Subsequences.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        vector<long long> b(7);
+
+        for (int i = 0; i < 7; i++)
+            cin >> b[i];
+
+        long long a = b[0];
+        long long x = b[1];
+        long long c;
+
+        if (b[2] == a + x)
+            c = b[6] - a - x;
+        else
+            c = b[2];
+
+        cout << a << " " << x << " " << c << "\n";
+    }
+
+    return 0;
+}
