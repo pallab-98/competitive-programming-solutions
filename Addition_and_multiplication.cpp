b90a03cf64675d49db716a4c5251a0b96c716486
Pallab <mazharulislampallab98@gmail.com>
2026-07-20 11:57:51 +0600

    CodeForces Problem Solve 01, 19/07/26

--- Addition_and_multiplication.cpp
+++ Addition_and_multiplication.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr)
+    cin.tie(nullptr);
+    
     int caseTest;
     cin >> caseTest;
     while (caseTest--)
