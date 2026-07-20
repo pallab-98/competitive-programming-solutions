7dee973bc56e6bc722c622c931887b07a6c70598
Pallab <mazharulislampallab98@gmail.com>
2026-07-20 12:14:57 +0600

    CodeForces Problem solve 03, 20/07/26

---
+++ A_Opponents.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, d;
+    cin >> n >> d;
+
+    int current = 0;
+    int maximum = 0;
+
+    while (d--) {
+        string s;
+        cin >> s;
+
+        bool absent = false;
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '0') {
+                absent = true;
+                break;
+            }
+        }
+
+        if (absent) {
+            current++;
+            if (current > maximum)
+                maximum = current;
+        } else {
+            current = 0;
+        }
+    }
+
+    cout << maximum << endl;
+
+    return 0;
+}
